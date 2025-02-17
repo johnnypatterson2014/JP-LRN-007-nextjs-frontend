752d09012a51fd96467ccbd823aad8f2c8bc2d2c
johnnypatterson2014 <johnny.patterson@gmail.com>
2025-02-16 16:06:13 -0800

    latest working code

--- src/app/pdf/page.tsx
+++ src/app/pdf/page.tsx
@@ -4,12 +4,12 @@ import PDFQuery from '@/components/pdf/PDFQuery'
 import { fetchPDFData, handleDeletePdf } from '@/lib/PDFActions';
 
 export default function Page() {
-  return (
-    <div id="main" className="container max-w-screen-md mx-auto m-5 p-2 border border-gray-500 rounded shadow bg-gray-100"> 
-        
-        <PDFList />
-        <PDFQuery />
+    return (
+        <div id="main" className="container max-w-screen-md mx-auto m-5 p-2 border border-gray-500 rounded shadow bg-gray-100">
 
-    </div>
-  );
+            <PDFList />
+            <PDFQuery />
+
+        </div>
+    );
 }
--- src/lib/PDFActions.ts
+++ src/lib/PDFActions.ts
@@ -1,32 +1,12 @@
-'use server'
+import { PDFData } from "@/lib/definitions";
 
-export async function fetchPDFData(setPdfs: (arg0: any) => void) {
+export async function fetchPDFData() {
     let path = '/pdfs';
     const response = await fetch(process.env.NEXT_PUBLIC_API_URL + path);
     const pdfData = await response.json()
-    setPdfs(pdfData);
-    // console.log("testing");
-    // return pdfData.map((pdf: PDFData) => ({
-    //     id: String(pdf.id),
-    //     name: String(pdf.name),
-    //     file: String(pdf.file)
-    //   }))
-}
-
-interface PDFData {
-    id: string;
-    name: string;
-    file: string;
-  };
-
-export async function handleDeletePdf(id: any, pdfs: PDFData[], setPdfs: (arg0: any) => void) {
-    const res = await fetch(process.env.NEXT_PUBLIC_API_URL + `/pdfs/${id}`, {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    if (res.ok) {
-      const copy = pdfs.filter((pdf) => pdf.id !== id);
-      setPdfs(copy);
-    }
+    return pdfData.map((pdf: PDFData) => ({
+        id: String(pdf.id),
+        name: String(pdf.name),
+        file: String(pdf.file)
+    }))
 }
